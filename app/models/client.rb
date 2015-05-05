4b032c8ffd6d24d9b225f7b1c2a341b93e8b19aa
Vagios Vlachos <bagios234@gmail.com>
2015-05-05 22:08:10 +0300

    Add generate_api_key for client

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,13 @@
 class Client < ActiveRecord::Base
+
+	def generate_api_key
+		loop do
+			token = SecureRandom.base64.tr('+/=', 'Qrt')
+			break token unless User.exists?(api_key: token).any?
+		end
+	end
+
+	before_create do |doc|
+		doc.api_key = doc.generate_api_key
+	end
 end
